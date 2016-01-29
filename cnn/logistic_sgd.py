309d8332db40e781ef6aaba61d7d02798e866230
HaohanWang <haohanw@andrew.cmu.edu>
2016-01-29 14:32:40 -0500

    update model

--- cnn/logistic_sgd.py
+++ cnn/logistic_sgd.py
@@ -47,32 +47,7 @@ import theano.tensor as T
 
 
 class LogisticRegression(object):
-    """Multi-class Logistic Regression Class
-
-    The logistic regression is fully described by a weight matrix :math:`W`
-    and bias vector :math:`b`. Classification is done by projecting data
-    points onto a set of hyperplanes, the distance to which is used to
-    determine a class membership probability.
-    """
-
     def __init__(self, input, n_in, n_out):
-        """ Initialize the parameters of the logistic regression
-
-        :type input: theano.tensor.TensorType
-        :param input: symbolic variable that describes the input of the
-                      architecture (one minibatch)
-
-        :type n_in: int
-        :param n_in: number of input units, the dimension of the space in
-                     which the datapoints lie
-
-        :type n_out: int
-        :param n_out: number of output units, the dimension of the space in
-                      which the labels lie
-
-        """
-        # start-snippet-1
-        # initialize with 0 the weights W as a matrix of shape (n_in, n_out)
         self.W = theano.shared(
             value=numpy.zeros(
                 (n_in, n_out),
@@ -81,7 +56,6 @@ class LogisticRegression(object):
             name='W',
             borrow=True
         )
-        # initialize the biases b as a vector of n_out 0s
         self.b = theano.shared(
             value=numpy.zeros(
                 (n_out,),
@@ -90,86 +64,63 @@ class LogisticRegression(object):
             name='b',
             borrow=True
         )
-
-        # symbolic expression for computing the matrix of class-membership
-        # probabilities
-        # Where:
-        # W is a matrix where column-k represent the separation hyperplane for
-        # class-k
-        # x is a matrix where row-j  represents input training sample-j
-        # b is a vector where element-k represent the free parameter of
-        # hyperplane-k
         self.p_y_given_x = T.nnet.softmax(T.dot(input, self.W) + self.b)
-
-        # symbolic description of how to compute prediction as class whose
-        # probability is maximal
         self.y_pred = T.argmax(self.p_y_given_x, axis=1)
-        # end-snippet-1
-
-        # parameters of the model
         self.params = [self.W, self.b]
-
-        # keep track of model input
         self.input = input
 
     def negative_log_likelihood(self, y):
-        """Return the mean of the negative log-likelihood of the prediction
-        of this model under a given target distribution.
-
-        .. math::
-
-            \frac{1}{|\mathcal{D}|} \mathcal{L} (\theta=\{W,b\}, \mathcal{D}) =
-            \frac{1}{|\mathcal{D}|} \sum_{i=0}^{|\mathcal{D}|}
-                \log(P(Y=y^{(i)}|x^{(i)}, W,b)) \\
-            \ell (\theta=\{W,b\}, \mathcal{D})
-
-        :type y: theano.tensor.TensorType
-        :param y: corresponds to a vector that gives for each example the
-                  correct label
-
-        Note: we use the mean instead of the sum so that
-              the learning rate is less dependent on the batch size
-        """
-        # start-snippet-2
-        # y.shape[0] is (symbolically) the number of rows in y, i.e.,
-        # number of examples (call it n) in the minibatch
-        # T.arange(y.shape[0]) is a symbolic vector which will contain
-        # [0,1,2,... n-1] T.log(self.p_y_given_x) is a matrix of
-        # Log-Probabilities (call it LP) with one row per example and
-        # one column per class LP[T.arange(y.shape[0]),y] is a vector
-        # v containing [LP[0,y[0]], LP[1,y[1]], LP[2,y[2]], ...,
-        # LP[n-1,y[n-1]]] and T.mean(LP[T.arange(y.shape[0]),y]) is
-        # the mean (across minibatch examples) of the elements in v,
-        # i.e., the mean log-likelihood across the minibatch.
         return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
-        # end-snippet-2
 
     def errors(self, y):
-        """Return a float representing the number of errors in the minibatch
-        over the total number of examples of the minibatch ; zero one
-        loss over the size of the minibatch
-
-        :type y: theano.tensor.TensorType
-        :param y: corresponds to a vector that gives for each example the
-                  correct label
-        """
-
-        # check if y has same dimension of y_pred
         if y.ndim != self.y_pred.ndim:
             raise TypeError(
                 'y should have the same shape as self.y_pred',
                 ('y', y.type, 'y_pred', self.y_pred.type)
             )
-        # check if y is of the correct datatype
         if y.dtype.startswith('int'):
-            # the T.neq operator returns a vector of 0s and 1s, where 1
-            # represents a mistake in prediction
             return T.mean(T.neq(self.y_pred, y))
         else:
             raise NotImplementedError()
 
+class LinearRegression(object):
+    def __init__(self, input, n_in, n_out):
+        self.W = theano.shared(
+            value=numpy.zeros(
+                (n_in, n_out),
+                dtype=theano.config.floatX
+            ),
+            name='W',
+            borrow=True
+        )
+        self.b = theano.shared(
+            value=numpy.zeros(
+                (n_out,),
+                dtype=theano.config.floatX
+            ),
+            name='b',
+            borrow=True
+        )
+        self.p_y_given_x = T.nnet.softmax(T.dot(input, self.W) + self.b)
+        self.y_pred = T.dot(input, self.W) + self.b
+        self.params = [self.W, self.b]
+        self.input = input
+
+    def mse(self, y):
+        return ((self.y_pred - y)**2).mean()
 
 def load_data(cv=1):
+    def shared_dataset(data_x_l, data_y, borrow=True):
+        shared_x_L = []
+        for data_x in data_x_l:
+            shared_x = theano.shared(numpy.asarray(data_x,
+                                                   dtype=theano.config.floatX),
+                                     borrow=borrow)
+            shared_x_L.append(shared_x)
+        shared_y = theano.shared(numpy.asarray(data_y,
+                                               dtype=theano.config.floatX),
+                                 borrow=borrow)
+        return shared_x_L, T.cast(shared_y, 'int32')
 
     trda = numpy.loadtxt('/media/haohanwang/DATA/BEST of Best/State Of Art/PPI4/NetworkHuman2/data/split/data_train_'+str(cv)+'_a.txt', delimiter=',')
     trdb = numpy.loadtxt('/media/haohanwang/DATA/BEST of Best/State Of Art/PPI4/NetworkHuman2/data/split/data_train_'+str(cv)+'_b.txt', delimiter=',')
@@ -180,36 +131,19 @@ def load_data(cv=1):
     trl = [int(line.strip()) for line in open('/media/haohanwang/DATA/BEST of Best/State Of Art/PPI4/NetworkHuman2/data/split/labels_train_'+str(cv)+'.txt')]
     tel = [int(line.strip()) for line in open('/media/haohanwang/DATA/BEST of Best/State Of Art/PPI4/NetworkHuman2/data/split/labels_test_'+str(cv)+'.txt')]
 
-    def shared_dataset(data_x, data_y, borrow=True):
-        """ Function that loads the dataset into shared variables
+    tr_bp = numpy.loadtxt('/media/haohanwang/DATA/BEST of Best/State Of Art/PPI4/NetworkHuman2/data/split/BP_train_'+str(cv)+'.txt', delimiter=',')
+    te_bp = numpy.loadtxt('/media/haohanwang/DATA/BEST of Best/State Of Art/PPI4/NetworkHuman2/data/split/BP_test_'+str(cv)+'.txt',delimiter=',')
 
-        The reason we store our dataset in shared variables is to allow
-        Theano to copy it into the GPU memory (when code is run on GPU).
-        Since copying data into the GPU is slow, copying a minibatch everytime
-        is needed (the default behaviour if the data is not in a shared
-        variable) would lead to a large decrease in performance.
-        """
-        shared_x = theano.shared(numpy.asarray(data_x,
-                                               dtype=theano.config.floatX),
-                                 borrow=borrow)
-        shared_y = theano.shared(numpy.asarray(data_y,
-                                               dtype=theano.config.floatX),
-                                 borrow=borrow)
-        # When storing data on the GPU it has to be stored as floats
-        # therefore we will store the labels as ``floatX`` as well
-        # (``shared_y`` does exactly that). But during our computations
-        # we need them as ints (we use labels as index, and if they are
-        # floats it doesn't make sense) therefore instead of returning
-        # ``shared_y`` we will have to cast it to int. This little hack
-        # lets ous get around this issue
-        return shared_x, T.cast(shared_y, 'int32')
+    tr_cc = numpy.loadtxt('/media/haohanwang/DATA/BEST of Best/State Of Art/PPI4/NetworkHuman2/data/split/CC_train_'+str(cv)+'.txt',delimiter=',')
+    te_cc = numpy.loadtxt('/media/haohanwang/DATA/BEST of Best/State Of Art/PPI4/NetworkHuman2/data/split/CC_train_'+str(cv)+'.txt',delimiter=',')
 
-    train_set_x1, train_set_y = shared_dataset(trda, trl)
-    train_set_x2, train_set_y = shared_dataset(trdb, trl)
-    valid_set_x1, valid_set_y = shared_dataset(teda,tel)
-    valid_set_x2, valid_set_y = shared_dataset(tedb,tel)
+    tr_mf = numpy.loadtxt('/media/haohanwang/DATA/BEST of Best/State Of Art/PPI4/NetworkHuman2/data/split/MF_train_'+str(cv)+'.txt',delimiter=',')
+    te_mf = numpy.loadtxt('/media/haohanwang/DATA/BEST of Best/State Of Art/PPI4/NetworkHuman2/data/split/MF_train_'+str(cv)+'.txt',delimiter=',')
 
-    rval = [(train_set_x1, train_set_x2, train_set_y), (valid_set_x1, valid_set_x2, valid_set_y)]
+    [train_set_x1, train_set_x2, train_bp, train_cc, train_mf], train_set_y = shared_dataset([trda, trdb, tr_bp, tr_cc, tr_mf], trl)
+    [test_set_x1, test_set_x2, test_bp, test_cc, test_mf], test_set_y = shared_dataset([teda, tedb, te_bp, te_cc, te_mf], tel)
+
+    rval = [(train_set_x1, train_set_x2, train_set_y, train_bp, train_cc, train_mf), (test_set_x1, test_set_x2, test_set_y, test_bp, test_cc, test_mf)]
     return rval
 
 
--- cnn/mlp.py
+++ cnn/mlp.py
@@ -4,26 +4,22 @@ import timeit
 import numpy
 import theano
 import theano.tensor as T
-from logistic_sgd import LogisticRegression, load_data
+from logistic_sgd import LogisticRegression, load_data, LinearRegression
 from cnn import LeNetConvPoolLayer
 from optimizers import Optimizer
-
 
 
 def tanh(x):
     return T.tanh(x)
 
-
 def rectifier(x):
     return T.maximum(0., x)
-
 
 def linear(x):
     return x
 
 def sigmoid(x):
     return T.nnet.sigmoid(x)
-
 
 class HiddenLayer(object):
     def __init__(self, rng, n_in, n_out, W=None, b=None,
@@ -60,8 +56,6 @@ class HiddenLayer(object):
             else self.activation(lin_output)
         )
 
-
-
 class DropoutHiddenLayer(HiddenLayer):
     def __init__(self, rng, n_in, n_out,
                  activation, dropout_rate, W=None, b=None):
@@ -95,69 +89,112 @@ class DropoutHiddenLayer(HiddenLayer):
             return r
 
 
-
-
 class MLP(object):
-    def __init__(self, rng, input1, input2, configs=(), n_out=2, batch_size=3):
+    def __init__(self, rng, input1, input2, configs=(), n_out=2, batch_size=3, activation=linear):
         self.hiddenLayer1 = DropoutHiddenLayer(
             rng=rng,
             n_in=configs[0],
             n_out=configs[1],
-            activation=tanh,
+            activation=activation,
             dropout_rate=-1
         )
 
-        # self.hiddenLayer2 = HiddenLayer(
-        #     rng=rng,
-        #     n_in=configs[1],
-        #     n_out=configs[2],
-        #     activation=sigmoid
-        # )
+        self.hiddenLayer2 = HiddenLayer(
+            rng=rng,
+            n_in=configs[1],
+            n_out=configs[2],
+            activation=activation
+        )
 
         out1_a = self.hiddenLayer1.output(input1)
         out1_b = self.hiddenLayer1.output(input2)
 
-        # out2_a = self.hiddenLayer2.output(out1_a)
-        # out2_b = self.hiddenLayer2.output(out1_b)
+        out2_a = self.hiddenLayer2.output(out1_a)
+        out2_b = self.hiddenLayer2.output(out1_b)
 
-        # h_output = [out1_a, out1_b]
-        # shp = out1_a.shape
-        # h_output = T.reshape(h_output, [shp[0], shp[1] * 2])
+        h_output = [out2_a, out2_b]
+        shp = out2_a.shape
+        h_output = T.reshape(h_output, [shp[0], shp[1] * 2])
 
-        self.out1 = out1_a
-        self.out2 = out1_b
-
-        h_output = out1_a * out1_b
-
-        covpol_input = h_output.reshape((batch_size, 1, 8, 8))
-
-        # TODO: free variables, batchsize = 20, nkerns = 4
-        self.CovPol = LeNetConvPoolLayer(
-            rng,
-            input=covpol_input,
-            image_shape=(batch_size, 1, 8, 8),
-            filter_shape=(4, 1, 3, 3),
-            poolsize=(2, 2)
-        )
-
-        covpol_output = self.CovPol.output.flatten(2)
+        self.out1 = out2_a
+        self.out2 = out2_b
+        #
+        # h_output = out1_a * out1_b
+        #
+        # covpol_input = h_output.reshape((batch_size, 1, 1, configs[1]))
+        #
+        # # TODO: free variables, batchsize = 20, nkerns = 4
+        # self.CovPol = LeNetConvPoolLayer(
+        #     rng,
+        #     input=covpol_input,
+        #     image_shape=(batch_size, 1, 1, configs[1]),
+        #     filter_shape=(4, 1, 1, 3),
+        #     poolsize=(1, 3)
+        # )
+        #
+        # covpol_output = self.CovPol.output.flatten(2)
 
         self.logRegressionLayer = LogisticRegression(
-            input=covpol_output,
-            n_in=36,
+            input=h_output,
+            n_in=configs[2]*2,
             n_out=n_out
         )
+
+        # self.CovPol_GO = LeNetConvPoolLayer(
+        #     rng,
+        #     input=covpol_input,
+        #     image_shape=(batch_size, 1, 1, configs[1]),
+        #     filter_shape=(4, 1, 1, 3),
+        #     poolsize=(1, 6)
+        # )
+        #
+        # covpol_output_go = self.CovPol_GO.output.flatten(2)
+        # covpol_output_go = covpol_output
+
+        self.regressionLayer_BP = LinearRegression(
+            input=h_output,
+            n_in=configs[2]*2,
+            n_out=5
+        )
+
+        self.regressionLayer_MF = LinearRegression(
+            input=h_output,
+            n_in=configs[2]*2,
+            n_out=5
+        )
+
+        self.regressionLayer_CC = LinearRegression(
+            input=h_output,
+            n_in=configs[2]*2,
+            n_out=5
+        )
+
         self.L1 = (
-            abs(self.hiddenLayer1.W).sum()  # + abs(self.hiddenLayer2.W).sum()
+            abs(self.hiddenLayer1.W).sum()
+            + abs(self.hiddenLayer2.W).sum()
+            #+ abs(self.CovPol.W).sum()
             + abs(self.logRegressionLayer.W).sum()
+            #+ abs(self.CovPol_GO.W).sum()
+            + abs(self.regressionLayer_BP.W).sum()
+            + abs(self.regressionLayer_MF.W).sum()
+            + abs(self.regressionLayer_CC.W).sum()
         )
         self.L2_sqr = (
-            (self.hiddenLayer1.W ** 2).sum()  # + (self.hiddenLayer2.W ** 2).sum()
+            (self.hiddenLayer1.W ** 2).sum()
+            + (self.hiddenLayer2.W**2).sum()
+            # + (self.CovPol.W ** 2).sum()
             + (self.logRegressionLayer.W ** 2).sum()
+            #+ (self.CovPol_GO.W**2).sum()
+            + (self.regressionLayer_BP.W**2).sum()
+            + (self.regressionLayer_MF.W**2).sum()
+            + (self.regressionLayer_CC.W).sum()
         )
 
         self.errors = self.logRegressionLayer.errors
-        self.params = self.hiddenLayer1.params + self.logRegressionLayer.params
+        self.params = self.hiddenLayer1.params  + self.hiddenLayer2.params \
+                      +self.logRegressionLayer.params \
+                      + self.regressionLayer_BP.params + self.regressionLayer_CC.params + self.regressionLayer_MF.params \
+                      #+ self.CovPol_GO.params
         # self.params = self.hiddenLayer1.params + self.logRegressionLayer.params
 
     def negative_log_likelihood(self, y):
@@ -166,16 +203,27 @@ class MLP(object):
     def distance(self, y):
         m = T.sum(T.square(self.out1 - self.out2), axis=1)
         s = T.dot(m, y) + 1e-3/(T.dot(m, -(y-1))+1e-3)
-        # s = T.dot(m, y) - T.dot(m, -(y-1))
+        # a = self.out1 - self.out1.mean()
+        # b = self.out2 - self.out2.mean()
+        # m = a*b/T.sqrt((a**2).sum()*(b**2).sum())
+        # s = T.dot(m, y) + 1e-3/(T.dot(m, -(y-1))+1e-3)
         return s
 
+    def mse_bp(self, y):
+        return self.regressionLayer_BP.mse(y)
+
+    def mse_mf(self, y):
+        return self.regressionLayer_MF.mse(y)
+
+    def mse_cc(self, y):
+        return self.regressionLayer_CC.mse(y)
 
 
-def test_mlp(learning_rate=0.1, L1_reg=0., L2_reg=0., D_reg=1.0, n_epochs=1000, batch_size=1000, cv=1):
+def test_mlp(learning_rate=0.1, L1_reg=0., L2_reg=0., D_reg=1.0, BP_reg=0.0, CC_reg=0.0, MF_reg=0.0, n_epochs=1000, batch_size=1000, cv=1):
     datasets = load_data(cv)
 
-    train_set_x1, train_set_x2, train_set_y = datasets[0]
-    valid_set_x1, valid_set_x2, valid_set_y = datasets[1]
+    train_set_x1, train_set_x2, train_set_y, train_bp, train_cc, train_mf = datasets[0]
+    valid_set_x1, valid_set_x2, valid_set_y, valid_bp, valid_cc, valid_mf = datasets[1]
 
     n_train_batches = train_set_x1.get_value(borrow=True).shape[0] / batch_size
     n_valid_batches = valid_set_x1.get_value(borrow=True).shape[0] / batch_size
@@ -187,31 +235,43 @@ def test_mlp(learning_rate=0.1, L1_reg=0., L2_reg=0., D_reg=1.0, n_epochs=1000, 
     x2 = T.matrix('x2')
     y = T.ivector('y')
 
+    y_bp = T.matrix('bp')
+    y_cc = T.matrix('cc')
+    y_mf = T.matrix('mf')
+
     rng = numpy.random.RandomState(1234)
 
     classifier = MLP(
         rng=rng,
         input1=x1,
         input2=x2,
-        configs=(158, 64, 5),
+        configs=(158, 100, 50),
         n_out=2,
-        batch_size=batch_size
+        batch_size=batch_size,
+        activation=rectifier
     )
 
     cost = (
         classifier.negative_log_likelihood(y)
         + L1_reg * classifier.L1
         + L2_reg * classifier.L2_sqr
-        + D_reg* classifier.distance(y)
+        + D_reg * classifier.distance(y)/batch_size
+        + BP_reg * classifier.mse_bp(y_bp)
+        + CC_reg * classifier.mse_cc(y_cc)
+        + MF_reg * classifier.mse_mf(y_mf)
     )
 
     validate_model = theano.function(
         inputs=[index],
-        outputs=(classifier.errors(y), classifier.distance(y)),
+        outputs=(classifier.errors(y), classifier.distance(y)/batch_size,
+                 classifier.mse_bp(y_bp), classifier.mse_cc(y_cc), classifier.mse_mf(y_mf)),
         givens={
             x1: valid_set_x1[index * batch_size:(index + 1) * batch_size],
             x2: valid_set_x2[index * batch_size:(index + 1) * batch_size],
-            y: valid_set_y[index * batch_size:(index + 1) * batch_size]
+            y: valid_set_y[index * batch_size:(index + 1) * batch_size],
+            y_bp: valid_bp[index * batch_size:(index + 1) * batch_size],
+            y_mf: valid_mf[index * batch_size:(index + 1) * batch_size],
+            y_cc: valid_cc[index * batch_size:(index + 1) * batch_size]
         }
     )
 
@@ -231,7 +291,10 @@ def test_mlp(learning_rate=0.1, L1_reg=0., L2_reg=0., D_reg=1.0, n_epochs=1000, 
         givens={
             x1: train_set_x1[index * batch_size: (index + 1) * batch_size],
             x2: train_set_x2[index * batch_size: (index + 1) * batch_size],
-            y: train_set_y[index * batch_size: (index + 1) * batch_size]
+            y: train_set_y[index * batch_size: (index + 1) * batch_size],
+            y_bp: train_bp[index * batch_size:(index + 1) * batch_size],
+            y_mf: train_mf[index * batch_size:(index + 1) * batch_size],
+            y_cc: train_cc[index * batch_size:(index + 1) * batch_size],
         }
     )
 
@@ -239,7 +302,7 @@ def test_mlp(learning_rate=0.1, L1_reg=0., L2_reg=0., D_reg=1.0, n_epochs=1000, 
 
     # early-stopping parameters
     patience = 1000  # look as this many examples regardless
-    patience_increase = 2  # wait this much longer when a new best is
+    patience_increase = 5  # wait this much longer when a new best is
     # found
     improvement_threshold = 0.995  # a relative improvement of this much is
     # considered significant
@@ -247,15 +310,19 @@ def test_mlp(learning_rate=0.1, L1_reg=0., L2_reg=0., D_reg=1.0, n_epochs=1000, 
 
     best_validation_loss = numpy.inf
     best_validation_distance = numpy.inf
+    best_validation_mse_bp = numpy.inf
+    best_validation_mse_mf = numpy.inf
+    best_validation_mse_cc = numpy.inf
     best_iter = 0
     test_score = 0.
     start_time = timeit.default_timer()
 
     epoch = 0
     done_looping = False
-
+    minibatch_avg_cost = 0
     # best_script = open('mlp.py')
     while (epoch < n_epochs) and (not done_looping):
+        # print classifier.CovPol.params[-1].get_value(True)
         epoch = epoch + 1
         for minibatch_index in xrange(n_train_batches):
 
@@ -266,21 +333,18 @@ def test_mlp(learning_rate=0.1, L1_reg=0., L2_reg=0., D_reg=1.0, n_epochs=1000, 
                 # compute zero-one loss on validation set
                 validation_losses = [validate_model(i) for i
                                      in xrange(n_valid_batches)]
-                validation_loss, validation_distance = zip(*validation_losses)
+                validation_loss, validation_distance, mse_bp, mse_cc, mse_mf = zip(*validation_losses)
                 this_validation_loss = numpy.mean(validation_loss)
                 this_validation_distance = numpy.mean(validation_distance)
 
+                this_validation_mse_bp = numpy.mean(mse_bp)
+                this_validation_mse_cc = numpy.mean(mse_cc)
+                this_validation_mse_mf = numpy.mean(mse_mf)
 
-                print(
-                    'epoch %i, minibatch %i/%i, validation error %f %%, validation distance %f' %
-                    (
-                        epoch,
-                        minibatch_index + 1,
-                        n_train_batches,
-                        this_validation_loss * 100.,
-                        this_validation_distance
-                    )
-                )
+                print 'epoch', epoch, 'minibatch', str(minibatch_index + 1)+'/'+str(n_train_batches),
+                print 'validation error', this_validation_loss*100, '%'
+                print 'validation distance', this_validation_distance
+                print 'validation mse bp, cc, mf', this_validation_mse_bp, this_validation_mse_cc, this_validation_mse_mf
 
                 if this_validation_loss < best_validation_loss:
                     if (this_validation_loss < best_validation_loss *improvement_threshold):
@@ -295,15 +359,32 @@ def test_mlp(learning_rate=0.1, L1_reg=0., L2_reg=0., D_reg=1.0, n_epochs=1000, 
                 if this_validation_distance < best_validation_distance:
                     if (this_validation_distance < best_validation_distance *improvement_threshold):
                         patience = max(patience, iter * patience_increase)
-                    # f = open('../model/best_script_' + str(cv) + '.py', 'w')
-                    # f.writelines(best_script)
-                    # f.close()
+
+                    best_validation_distance = this_validation_distance
+
+                if this_validation_mse_bp < best_validation_mse_bp:
+                    if (this_validation_mse_bp < best_validation_mse_bp *improvement_threshold):
+                        patience = max(patience, iter * patience_increase)
+
+                    best_validation_mse_bp = this_validation_mse_bp
+
+                if this_validation_mse_cc < best_validation_mse_cc:
+                    if (this_validation_mse_cc < best_validation_mse_cc *improvement_threshold):
+                        patience = max(patience, iter * patience_increase)
+
+                    best_validation_mse_cc = this_validation_mse_cc
+
+                if this_validation_mse_mf < best_validation_mse_mf:
+                    if (this_validation_mse_mf < best_validation_mse_mf *improvement_threshold):
+                        patience = max(patience, iter * patience_increase)
+
+                    best_validation_mse_mf = this_validation_mse_mf
+
 
             if patience <= iter:
                 done_looping = True
                 break
         print minibatch_avg_cost
-        print classifier.params[-1].get_value(True)
         # print classifier.params[1].get_value(True)
 
     end_time = timeit.default_timer()
@@ -321,8 +402,14 @@ if __name__ == '__main__':
     # lr = float(args[1])
     # batch_size = int(args[2])
     lr = 0.001
-    batch_size = 400
+    batch_size = 1000
     l1 = 0
-    l2 = 1e-4
-    dr = 1e-6
-    test_mlp(cv=1, learning_rate=lr, L1_reg=l1, L2_reg=l2, D_reg=dr, batch_size=batch_size)
+    l2 = 0
+    dr = 0
+    bp_reg = 0
+    cc_reg = 0
+    mf_reg = 0
+    test_mlp(cv=1, learning_rate=lr, L1_reg=l1, L2_reg=l2, D_reg=dr, BP_reg=bp_reg, CC_reg=cc_reg, MF_reg=mf_reg,
+             batch_size=batch_size)
+    print l1, l2, dr, bp_reg, cc_reg, mf_reg
+    # covPol as second layer best 37.74: l1=1e-1, l2=1e-2, dr=0
